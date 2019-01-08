15a7701c2428c6c272d7a92d74ceff9e753003d7
Thomas Geissl <thomas.geissl@gmail.com>
2019-01-07 20:36:11 -0500

    fixed hash

--- ofpackagemanager.rb
+++ ofpackagemanager.rb
@@ -3,7 +3,7 @@ class Ofpackagemanager < Formula
   homepage "https://github.com/thomasgeissl/ofPackageManager"
   url "https://github.com/thomasgeissl/ofPackageManager/releases/download/0.0.1/ofPackageManager_osx_0.0.1.zip"
   version "0.0.1"
-  sha256 "5b72e72f1c2678d074b7f33bf3da8cff6b27c161de7e46957bd5ab9d56ea6606"
+  sha256 "7034a5fdc30242a553237c095a3d87e05caca1390747eecc20d8808c6d27cf2d"
 
   def install
     bin.install Dir["bin/*"]
