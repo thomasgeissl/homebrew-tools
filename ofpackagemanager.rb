7d663abc5a6afd307874727d2978f7615b9960d9
Thomas Geissl <thomas.geissl@gmail.com>
2019-01-21 04:10:17 -0500

    Update ofpackagemanager.rb

--- ofpackagemanager.rb
+++ ofpackagemanager.rb
@@ -1,9 +1,9 @@
 class Ofpackagemanager < Formula
   desc "package manager for openFrameworks"
   homepage "https://github.com/thomasgeissl/ofPackageManager"
-  url "https://github.com/thomasgeissl/ofPackageManager/releases/download/0.0.1/ofPackageManager_osx_0.0.1.zip"
-  version "0.0.1"
-  sha256 "7034a5fdc30242a553237c095a3d87e05caca1390747eecc20d8808c6d27cf2d"
+  url "https://github.com/thomasgeissl/ofPackageManager/releases/download/0.0.2/ofPackageManager_osx_0.0.2.zip"
+  version "0.0.2"
+  sha256 "53d11337f20bd6d7320cf874565cfec0b13b4411208128b7d36eaecf91b0630e"
 
   def install
     bin.install Dir["bin/*"]
