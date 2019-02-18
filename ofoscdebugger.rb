5775e6ced3ace0f1999d331dfcaac41ebf26c45e
Thomas Geissl <thomas.geissl@gmail.com>
2019-02-18 12:49:42 -0400

    fixed url

--- ofoscdebugger.rb
+++ ofoscdebugger.rb
@@ -1,7 +1,7 @@
 class Ofoscdebugger < Formula
   desc "osc monitor and sender"
   homepage "https://github.com/thomasgeissl/ofOSCDebugger"
-  url "https://github.com/thomasgeissl/ofOSCDebugger/releases/download/1.0.0/ofOSCDebugger.1.0.0.zip"
+  url "https://github.com/thomasgeissl/ofOSCDebugger/releases/download/1.0.0/ofOSCDebugger_osx_1.0.0.zip"
   version "1.0.0"
   sha256 "1592e3e0de90230fc3a41f1cb06efae2e81efbd6902cb5329e989572a7ec7a33"
 
