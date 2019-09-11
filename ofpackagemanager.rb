a383ff087acb602cecbfb57f91fab8ad00ee1406
Thomas Geissl <thomas.geissl@gmail.com>
2019-09-11 17:45:30 +0200

    fixed url

--- ofpackagemanager.rb
+++ ofpackagemanager.rb
@@ -1,8 +1,8 @@
 class Ofpackagemanager < Formula
-  version "0.0.12"
+  version '0.0.12'
   desc "package manager for openFrameworks"
   homepage "https://github.com/thomasgeissl/ofPackageManager"
-  url "https://github.com/thomasgeissl/ofPackageManager/releases/download/{version}/ofPackageManager_osx_{version}.zip"
+  url "https://github.com/thomasgeissl/ofPackageManager/releases/download/#{version}/ofPackageManager_osx_#{version}.zip"
   sha256 "018422cabc2f445d88ae7759a4856501308037fb297c1ff25868817c95a8e090"
 
   def install
