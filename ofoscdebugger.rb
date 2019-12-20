6e62643fc63a2a9fb2d413263802b32eab58ce65
Thomas Geissl <thomas.geissl@gmail.com>
2019-12-20 16:11:15 +0100

    Update ofoscdebugger.rb

--- ofoscdebugger.rb
+++ ofoscdebugger.rb
@@ -1,8 +1,8 @@
 class Ofoscdebugger < Formula
   desc "osc monitor and sender"
   homepage "https://github.com/thomasgeissl/ofOSCDebugger"
-  url "https://github.com/thomasgeissl/ofOSCDebugger/releases/download/#{version}/ofOSCDebugger_osx_#{version}.zip"
   version "1.2.0"
+  url "https://github.com/thomasgeissl/ofOSCDebugger/releases/download/#{version}/ofOSCDebugger_osx_#{version}.zip"
   sha256 "303dd285510b641a502cea63caa459a445c142fc767f66eff6a8b67f91464e78"
 
   def install
