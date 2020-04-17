e3c4c4b7ef357dea985a518a275efcd57217795e
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-17 09:47:35 +0200

    fixed install

--- ofprojectgenerator.rb
+++ ofprojectgenerator.rb
@@ -4,11 +4,10 @@ class Ofprojectgenerator < Formula
     homepage "https://github.com/thomasgeissl/projectGenerator"
     url "https://github.com/thomasgeissl/projectGenerator/releases/download/#{version}/commandline_osx_#{version}.zip"
     sha256 "810509f6f8119427fced03de3a62323035b235e6f73a837f76f7e4e94d48e016"
-  
+
     def install
       bin.install Dir["bin/*"]
       bin.install_symlink "projectGenerator.app/Contents/MacOS/projectGenerator"
-      ln_sf SOURCE, "#{HOMEBREW_PREFIX}/some/custom/target"
     end
   end
   
