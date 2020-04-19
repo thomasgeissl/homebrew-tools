68b7e3e40556deedb3c52492ba1b9a83ce738d73
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-19 09:21:02 +0200

    Update ofprojectgenerator.rb

--- ofprojectgenerator.rb
+++ ofprojectgenerator.rb
@@ -1,8 +1,8 @@
 class Ofprojectgenerator < Formula
     version '0.11.0'
     desc "project generator for openFrameworks"
-    homepage "https://github.com/thomasgeissl/projectGenerator"
-    url "https://github.com/thomasgeissl/projectGenerator/releases/download/#{version}/commandline_osx_#{version}.zip"
+    homepage "https://github.com/thomasgeissl/ofProjectGenerator"
+    url "https://github.com/thomasgeissl/ofProjectGenerator/releases/download/#{version}/commandline_osx_#{version}.zip"
     sha256 "810509f6f8119427fced03de3a62323035b235e6f73a837f76f7e4e94d48e016"
 
     def install
