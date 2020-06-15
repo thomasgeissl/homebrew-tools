b86c49fc8cf646817e438a949f7fed733a1aa9eb
Thomas Geissl <thomas.geissl@gmail.com>
2020-06-15 10:14:01 +0200

    Update ofpackagemanagerfrontend.rb

--- Casks/ofpackagemanagerfrontend.rb
+++ Casks/ofpackagemanagerfrontend.rb
@@ -1,8 +1,8 @@
 cask 'ofpackagemanagerfrontend' do
-    version '0.0.21-2'
+    version '0.0.21-3'
     homepage "https://github.com/thomasgeissl/ofPackageManager-frontend"
     url "https://github.com/thomasgeissl/ofPackageManager-frontend/releases/download/#{version}/ofPackageManager-#{version}.dmg"
-    sha256 "cc7b9738bc5189a71d609158995fd5cb7b5f59075bbf4d2817ee796bb18934ee"
+    sha256 "69af429f8a008a3e431e3d838b37e9d66f243ff70048cf83ff91cd849cc7b40d"
     appcast 'https://github.com/thomasgeissl/ofPackageManager-frontend/releases.atom'
     app "ofPackageManager.app"
 end
