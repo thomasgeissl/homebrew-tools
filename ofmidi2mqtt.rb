87601ef42ee7b830ff4d7b98664ed837c5da1ba1
Thomas Geissl <thomas.geissl@gmail.com>
2020-09-23 19:09:00 +0200

    updated hash

--- ofmidi2mqtt.rb
+++ ofmidi2mqtt.rb
@@ -3,9 +3,10 @@ class Ofmidi2mqtt < Formula
   desc "MIDI to MQTT bridge"
   homepage "https://github.com/thomasgeissl/ofMIDI2MQTT"
   url "https://github.com/thomasgeissl/ofMIDI2MQTT/releases/download/#{version}/ofMIDI2MQTT_osx_#{version}.zip"
-  sha256 "a43e01d44d76b81a6595a0a4e731faf7c4f36fa22839b2e364108abd11548096"
+  sha256 "e436d3bf1552694c3c019169147e89b3dafc3ac822f22b83c8ea8a1c0c2ff3d1"
 
   def install
+    bin.install Dir["bin/*"]
     bin.install_symlink "ofMIDI2MQTT.app/Contents/MacOS/ofMIDI2MQTT"
   end
 end
