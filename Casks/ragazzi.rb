2909a62f36bc40ccf3a87d0fcec61349fad8839a
Thomas Geissl <thomas.geissl@gmail.com>
2022-06-26 10:56:46 +0200

    fixed hash

--- Casks/ragazzi.rb
+++ Casks/ragazzi.rb
@@ -2,7 +2,7 @@ cask 'ragazzi' do
     version '0.5.1'
     homepage "https://github.com/thomasgeissl/ragazzi"
     url "https://github.com/thomasgeissl/ragazzi/releases/download/#{version}/ragazzi-#{version}.dmg"
-    sha256 "06c7eb0f19945183ffa15dc96e0c4303be8743662c2ec859151359eead844d12"
+    sha256 "a1b7d05edda2bb41e52b34678a39ad927847fb8478d9b06b2abf72b6415f85cf"
     appcast 'https://github.com/thomasgeissl/ragazzi/releases.atom'
     app "ragazzi.app"
 end
