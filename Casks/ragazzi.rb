c14cea5f2b5f80c183ecdfb243650078fd75388f
Thomas Geissl <thomas.geissl@gmail.com>
2022-08-23 19:22:12 +0200

    updated ragazzi

--- Casks/ragazzi.rb
+++ Casks/ragazzi.rb
@@ -1,8 +1,8 @@
 cask 'ragazzi' do
-    version '0.5.1'
+    version '0.6.0'
     homepage "https://github.com/thomasgeissl/ragazzi"
     url "https://github.com/thomasgeissl/ragazzi/releases/download/#{version}/ragazzi-#{version}.dmg"
-    sha256 "a1b7d05edda2bb41e52b34678a39ad927847fb8478d9b06b2abf72b6415f85cf"
+    sha256 "fd813a5072327e3bbd527a7fe34b23d7eea167b5d40fb2ec6435d19c077f7bb6"
     appcast 'https://github.com/thomasgeissl/ragazzi/releases.atom'
     app "ragazzi.app"
 end
