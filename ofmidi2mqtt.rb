f1e1c31d0d0530917f645a648bce3830ae862304
Thomas Geissl <thomas.geissl@gmail.com>
2023-09-23 10:49:46 +0200

    bumbed ofMIDI2MQTT minor version

--- ofmidi2mqtt.rb
+++ ofmidi2mqtt.rb
@@ -1,9 +1,9 @@
 class Ofmidi2mqtt < Formula
-  version '1.0.1'
+  version '1.1.0'
   desc "MIDI to MQTT bridge"
   homepage "https://github.com/thomasgeissl/ofMIDI2MQTT"
   url "https://github.com/thomasgeissl/ofMIDI2MQTT/releases/download/#{version}/ofMIDI2MQTT_osx_#{version}.zip"
-  sha256 "63bf3e3d7445eac8409f294ba42799657871393f32b16b02eca2416866985189"
+  sha256 "07ffbf0d9a773e2ccb561323a3384699cb76653ccf5787062b41f84dc8d208da"
 
   def install
     bin.install Dir["bin/*"]
