fec0c4993ab44fba4a0fa58e24d177e38ced0d5a
Thomas Geissl <thomas.geissl@gmail.com>
2024-02-04 13:48:13 +0100

    trying to fix appcast error ragazzi.rb

--- Casks/ragazzi.rb
+++ Casks/ragazzi.rb
@@ -3,6 +3,10 @@ cask 'ragazzi' do
     homepage "https://github.com/thomasgeissl/ragazzi"
     url "https://github.com/thomasgeissl/ragazzi/releases/download/#{version}/ragazzi-#{version}.dmg"
     sha256 "647361ce131e41988402255be77f2466bfd415aedef5b2b10cd459d541068c66"
-    appcast 'https://github.com/thomasgeissl/ragazzi/releases.atom'
+    livecheck do
+      url "https://github.com/thomasgeissl/ragazzi/releases.atom"
+      regex(%r{v?(\d+(?:\.\d+)+)</title>}i)
+      strategy :page_match
+    end
     app "ragazzi.app"
 end
